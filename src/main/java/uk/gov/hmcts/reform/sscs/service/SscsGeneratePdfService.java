691a0731577bbf222c323522b9c1f81b77df9ace
Mark Hunter <mark.hunter@agilesphere.co.uk>
2019-01-09 13:27:26 +0000

    SSCS-4616 - Temporary commit

--- src/main/java/uk/gov/hmcts/reform/sscs/service/SscsGeneratePdfService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/SscsGeneratePdfService.java
@@ -5,7 +5,9 @@ import static uk.gov.hmcts.reform.sscs.config.PDFTemplateConstants.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,19 +18,22 @@ import uk.gov.hmcts.reform.pdf.service.client.PDFServiceClient;
 import uk.gov.hmcts.reform.sscs.ccd.domain.*;
 import uk.gov.hmcts.reform.sscs.domain.pdf.PdfWrapper;
 import uk.gov.hmcts.reform.sscs.exception.PdfGenerationException;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 
 @Service
 @Slf4j
 public class SscsGeneratePdfService {
     private static final String RPC = "rpc_";
     private PDFServiceClient pdfServiceClient;
+    private PdfStoreService pdfStoreService;
 
     @Autowired
-    public SscsGeneratePdfService(PDFServiceClient pdfServiceClient) {
+    public SscsGeneratePdfService(PDFServiceClient pdfServiceClient, PdfStoreService pdfStoreService) {
         this.pdfServiceClient = pdfServiceClient;
+        this.pdfStoreService = pdfStoreService;
     }
 
-    public byte[] generatePdf(String templatePath, SscsCaseData sscsCaseData, Long caseDetailsId, Map<String, String> notificationPlaceholders) {
+    public byte[] generatePdf(String templatePath, SscsCaseData sscsCaseData, Long caseDetailsId, Map<String, String> notificationPlaceholders, IdamTokens idamTokens) {
         byte[] template;
         try {
             template = getTemplate(templatePath);
@@ -68,4 +73,31 @@ public class SscsGeneratePdfService {
         InputStream in = getClass().getResourceAsStream(templatePath);
         return IOUtils.toByteArray(in);
     }
+
+    public void mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData, IdamTokens idamTokens) {
+        List<SscsDocument> pdfDocuments = pdfStoreService.store(pdf, fileName);
+
+        log.info("Appeal PDF stored in DM for Nino - {} and benefit type {}", caseData.getAppeal().getAppellant().getIdentity().getNino(),
+            caseData.getAppeal().getBenefitType().getCode());
+
+        if (caseId == null) {
+            log.info("caseId is empty - skipping step to update CCD with PDF");
+        } else {
+            SscsDocument document = SscsDocument.builder().
+            List<SscsDocument> allDocuments = combineEvidenceAndAppealPdf(caseData, pdfDocuments);
+            SscsCaseData caseDataWithAppealPdf = caseData.toBuilder().sscsDocument(allDocuments).build();
+            updateCaseInCcd(caseDataWithAppealPdf, caseId, "uploadDocument", idamTokens);
+        }
+    }
+
+    private List<SscsDocument> combineEvidenceAndAppealPdf(SscsCaseData caseData, List<SscsDocument> pdfDocuments) {
+        List<SscsDocument> evidenceDocuments = caseData.getSscsDocument();
+        List<SscsDocument> allDocuments = new ArrayList<>();
+        if (evidenceDocuments != null) {
+            allDocuments.addAll(evidenceDocuments);
+        }
+        allDocuments.addAll(pdfDocuments);
+        return allDocuments;
+    }
+
 }
