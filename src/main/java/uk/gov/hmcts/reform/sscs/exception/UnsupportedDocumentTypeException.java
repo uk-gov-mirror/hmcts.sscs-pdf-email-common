abad803afdb0872c5f7d878e1e752a6887b3f5f0
Chris Moreton <christopher.moreton@hmcts.net>
2019-02-08 11:39:47 +0000

    Modify exceptions for new logging lib

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/UnsupportedDocumentTypeException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/UnsupportedDocumentTypeException.java
@@ -2,15 +2,13 @@ package uk.gov.hmcts.reform.sscs.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
 
 @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY,
         reason = "One of the given document type is not accepted by Document Store")
-public class UnsupportedDocumentTypeException extends UnknownErrorCodeException {
+public class UnsupportedDocumentTypeException extends RuntimeException {
 
     public UnsupportedDocumentTypeException(Throwable cause) {
-        super(AlertLevel.P4, cause);
+        super(cause.getMessage());
     }
 
 }
