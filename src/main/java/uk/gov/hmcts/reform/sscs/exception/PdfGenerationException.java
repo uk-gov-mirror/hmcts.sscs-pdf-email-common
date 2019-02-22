89a482f8e8799057cf12652a4104a2c16258a574
Chris Moreton <christopher.moreton@hmcts.net>
2019-02-22 11:16:31 +0000

    Bump java-logging library version

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/PdfGenerationException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/PdfGenerationException.java
@@ -2,14 +2,12 @@ package uk.gov.hmcts.reform.sscs.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR,
         reason = "Error while generating appeal pdf")
-public class PdfGenerationException extends UnknownErrorCodeException {
+public class PdfGenerationException extends RuntimeException {
     public PdfGenerationException(String message, Throwable cause) {
-        super(AlertLevel.P4, cause);
+        super(cause);
     }
 }
