edd8a50436125b5416860d7a239141abe4779fe8
jackmaloney <jack.maloney@transformuk.com>
2019-02-22 10:21:50 +0000

    Remove unknown error type exception

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/UnknownFileTypeException.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/exception/UnknownFileTypeException.java
@@ -1,12 +1,9 @@
 package uk.gov.hmcts.reform.sscs.exception;
 
-import uk.gov.hmcts.reform.logging.exception.AlertLevel;
-import uk.gov.hmcts.reform.logging.exception.UnknownErrorCodeException;
-
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class UnknownFileTypeException extends UnknownErrorCodeException {
+public class UnknownFileTypeException extends RuntimeException {
 
     public UnknownFileTypeException(String message, Throwable cause) {
-        super(AlertLevel.P3, cause);
+        super(cause);
     }
 }
