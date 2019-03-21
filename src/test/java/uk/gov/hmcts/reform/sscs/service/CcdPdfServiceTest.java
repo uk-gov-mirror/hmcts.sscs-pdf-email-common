aa7e2750bdc4c8effdd42f34a6a777832b0e2a75
jackmaloney <jack.maloney@transformuk.com>
2019-03-21 09:17:00 +0000

    Allow custom description into document upload event

--- src/test/java/uk/gov/hmcts/reform/sscs/service/CcdPdfServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/CcdPdfServiceTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.sscs.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.util.CaseDataUtils.buildCaseData;
@@ -38,6 +39,16 @@ public class CcdPdfServiceTest {
         service.mergeDocIntoCcd("Myfile.pdf", pdf,1L, caseData, IdamTokens.builder().build());
 
         verify(pdfStoreService).store(any(), any());
-        verify(ccdService).updateCase(any(), any(), any(), any(), any(), any());
+        verify(ccdService).updateCase(any(), any(), any(), eq("SSCS - upload document event"), eq("Uploaded document into SSCS"), any());
+    }
+
+    @Test
+    public void mergeValidPdfAndStoreInDocumentStoreWithDescription() {
+        byte[] pdf = {};
+
+        service.mergeDocIntoCcd("Myfile.pdf", pdf,1L, caseData, IdamTokens.builder().build(), "My description");
+
+        verify(pdfStoreService).store(any(), any());
+        verify(ccdService).updateCase(any(), any(), any(), eq("SSCS - upload document event"), eq("My description"), any());
     }
 }
