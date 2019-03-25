36c6fa4f4f0771b6529f8ff7b0b33191b257bd9a
jackmaloney <jack.maloney@transformuk.com>
2019-03-25 11:35:21 +0000

    Move template enum to each service config

--- src/main/java/uk/gov/hmcts/reform/sscs/docmosis/domain/Template.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/docmosis/domain/Template.java
@@ -1,13 +1,11 @@
 package uk.gov.hmcts.reform.sscs.docmosis.domain;
 
-public enum Template {
-
-    DL6("TB-SCS-GNO-ENG-00010 v0.2.doc", "DL6");
+public class Template {
 
     private String templateName;
     private String hmctsDocName;
 
-    Template(String templateName, String hmctsDocName) {
+    public Template(String templateName, String hmctsDocName) {
         this.templateName = templateName;
         this.hmctsDocName = hmctsDocName;
     }
