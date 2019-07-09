bafc335a944e9168524d95325b7c09704c6a89a6
jackmaloney <jack.maloney@transformuk.com>
2019-07-09 20:19:48 +0100

    Move allowed content types to common library

--- src/main/java/uk/gov/hmcts/reform/sscs/exception/UnknownFileTypeException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.sscs.exception;
-
-@SuppressWarnings("squid:MaximumInheritanceDepth")
-public class UnknownFileTypeException extends RuntimeException {
-
-    public UnknownFileTypeException(String message, Throwable cause) {
-        super(cause);
-    }
-}
