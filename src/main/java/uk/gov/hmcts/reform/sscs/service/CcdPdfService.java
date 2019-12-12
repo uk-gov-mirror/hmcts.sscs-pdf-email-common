89ee832ea59f0b04897dd6419fa59039bf9ee55f
david.crespo <david.crespo@hmcts.net>
2019-12-12 23:37:09 +0100

    add test scenario to merge in ccd a new appellant statement
    add prod code to pass it

--- src/main/java/uk/gov/hmcts/reform/sscs/service/CcdPdfService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/CcdPdfService.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.reform.sscs.service;
 
-import java.util.ArrayList;
+import static org.apache.commons.collections4.ListUtils.emptyIfNull;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.UPLOAD_DOCUMENT;
+
+import java.util.Collections;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.sscs.ccd.domain.ScannedDocument;
+import uk.gov.hmcts.reform.sscs.ccd.domain.ScannedDocumentDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocument;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocumentDetails;
 import uk.gov.hmcts.reform.sscs.ccd.exception.CcdException;
 import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
@@ -16,6 +23,8 @@ import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 @Slf4j
 public class CcdPdfService {
 
+    private static final String APPELLANT_STATEMENT = "Appellant statement ";
+
     @Autowired
     private PdfStoreService pdfStoreService;
 
@@ -23,48 +32,58 @@ public class CcdPdfService {
     private CcdService ccdService;
 
     // can be removed once COR team decide what to pass into the documentType field
-    public SscsCaseData mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData, IdamTokens idamTokens) {
-        return updateAndMerge(fileName, pdf, caseId, caseData, idamTokens, "Uploaded document into SSCS", null);
+    public SscsCaseData mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData,
+                                        IdamTokens idamTokens) {
+        return updateAndMerge(fileName, pdf, caseId, caseData, idamTokens, "Uploaded document into SSCS",
+            null);
     }
 
-    public SscsCaseData mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData, IdamTokens idamTokens, String documentType) {
-        return updateAndMerge(fileName, pdf, caseId, caseData, idamTokens, "Uploaded document into SSCS", documentType);
+    public SscsCaseData mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData,
+                                        IdamTokens idamTokens, String documentType) {
+        return updateAndMerge(fileName, pdf, caseId, caseData, idamTokens, "Uploaded document into SSCS",
+            documentType);
     }
 
-    public SscsCaseData mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData, IdamTokens idamTokens, String description, String documentType) {
+    public SscsCaseData mergeDocIntoCcd(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData,
+                                        IdamTokens idamTokens, String description, String documentType) {
         return updateAndMerge(fileName, pdf, caseId, caseData, idamTokens, description, documentType);
     }
 
-    private SscsCaseData updateAndMerge(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData, IdamTokens idamTokens, String description, String documentType) {
+    private SscsCaseData updateAndMerge(String fileName, byte[] pdf, Long caseId, SscsCaseData caseData,
+                                        IdamTokens idamTokens, String description, String documentType) {
         List<SscsDocument> pdfDocuments = pdfStoreService.store(pdf, fileName, documentType);
 
         log.info("Case {} PDF stored in DM for benefit type {}", caseId,
-                caseData.getAppeal().getBenefitType().getCode());
+            caseData.getAppeal().getBenefitType().getCode());
 
         if (caseId == null) {
             log.info("caseId is empty - skipping step to update CCD with PDF");
+            return caseData;
+        }
+        if (fileName.startsWith(APPELLANT_STATEMENT)) {
+            SscsDocumentDetails pdfDocDetails = pdfDocuments.get(0).getValue();
+            ScannedDocument newScannedDoc = ScannedDocument.builder()
+                .value(ScannedDocumentDetails.builder()
+                    .fileName(pdfDocDetails.getDocumentFileName())
+                    .url(pdfDocDetails.getDocumentLink())
+                    .type("other")
+                    .build())
+                .build();
+            caseData.setScannedDocuments(ListUtils.union(emptyIfNull(caseData.getScannedDocuments()),
+                Collections.singletonList(newScannedDoc)));
         } else {
-            List<SscsDocument> allDocuments = combineEvidenceAndAppealPdf(caseData, pdfDocuments);
-            caseData.setSscsDocument(allDocuments);
-            SscsCaseDetails caseDetails = updateCaseInCcd(caseData, caseId, "uploadDocument", idamTokens, description);
-            return caseDetails.getData();
+            caseData.setSscsDocument(ListUtils.union(emptyIfNull(caseData.getSscsDocument()),
+                emptyIfNull(pdfDocuments)));
         }
-        return caseData;
+        return updateCaseInCcd(caseData, caseId, UPLOAD_DOCUMENT.getCcdType(), idamTokens,
+            description).getData();
     }
 
-    private List<SscsDocument> combineEvidenceAndAppealPdf(SscsCaseData caseData, List<SscsDocument> pdfDocuments) {
-        List<SscsDocument> evidenceDocuments = caseData.getSscsDocument();
-        List<SscsDocument> allDocuments = new ArrayList<>();
-        if (evidenceDocuments != null) {
-            allDocuments.addAll(evidenceDocuments);
-        }
-        allDocuments.addAll(pdfDocuments);
-        return allDocuments;
-    }
-
-    private SscsCaseDetails updateCaseInCcd(SscsCaseData caseData, Long caseId, String eventId, IdamTokens idamTokens, String description) {
+    private SscsCaseDetails updateCaseInCcd(SscsCaseData caseData, Long caseId, String eventId, IdamTokens idamTokens,
+                                            String description) {
         try {
-            return ccdService.updateCase(caseData, caseId, eventId, "SSCS - upload document event", description, idamTokens);
+            return ccdService.updateCase(caseData, caseId, eventId, "SSCS - upload document event",
+                description, idamTokens);
         } catch (CcdException ccdEx) {
             log.error("Failed to update ccd case but carrying on [" + caseId + "] ["
                     + caseData.getCaseReference() + "] with event [" + eventId + "]", ccdEx);
--- src/test/java/uk/gov/hmcts/reform/sscs/service/CcdPdfServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/CcdPdfServiceTest.java
@@ -1,19 +1,33 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.UPLOAD_DOCUMENT;
 import static uk.gov.hmcts.reform.sscs.ccd.util.CaseDataUtils.buildCaseData;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import uk.gov.hmcts.reform.sscs.ccd.domain.DocumentLink;
+import uk.gov.hmcts.reform.sscs.ccd.domain.ScannedDocument;
+import uk.gov.hmcts.reform.sscs.ccd.domain.ScannedDocumentDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseData;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
 import uk.gov.hmcts.reform.sscs.ccd.domain.SscsDocument;
@@ -32,7 +46,7 @@ public class CcdPdfServiceTest {
     @Mock
     CcdService ccdService;
 
-    SscsCaseData caseData = buildCaseData();
+    private SscsCaseData caseData = buildCaseData();
 
     @Before
     public void setup() {
@@ -42,17 +56,73 @@ public class CcdPdfServiceTest {
     @Test
     public void mergeValidPdfAndStoreInDocumentStore() {
         List<SscsDocument> sscsDocuments = new ArrayList<>();
-        sscsDocuments.add(SscsDocument.builder().value(SscsDocumentDetails.builder().documentFileName("Test.jpg").build()).build());
+        sscsDocuments.add(SscsDocument.builder()
+            .value(SscsDocumentDetails.builder()
+                .documentFileName("Test.jpg")
+                .build())
+            .build());
 
         when(pdfStoreService.store(any(), any(), eq("dl6"))).thenReturn(sscsDocuments);
-        when(ccdService.updateCase(any(), any(), any(), any(), any(), any())).thenReturn(SscsCaseDetails.builder().data(caseData).build());
+        when(ccdService.updateCase(any(), any(), any(), any(), any(), any()))
+            .thenReturn(SscsCaseDetails.builder().data(caseData).build());
 
         byte[] pdf = {};
-        service.mergeDocIntoCcd("Myfile.pdf", pdf,1L, caseData, IdamTokens.builder().build(), "dl6");
+        service.mergeDocIntoCcd("Myfile.pdf", pdf, 1L, caseData, IdamTokens.builder().build(),
+            "dl6");
 
         verify(pdfStoreService).store(any(), any(), eq("dl6"));
-        verify(ccdService).updateCase(any(), any(), any(), eq("SSCS - upload document event"), eq("Uploaded document into SSCS"), any());
+        verify(ccdService).updateCase(any(), any(), any(), eq("SSCS - upload document event"),
+            eq("Uploaded document into SSCS"), any());
         assertEquals("Test.jpg", caseData.getSscsDocument().get(0).getValue().getDocumentFileName());
+    }
+
+    @Test
+    public void givenAppellantStatement_shouldMergeDocIntoCcd() {
+        mockPdfStoreService();
+        when(ccdService.updateCase(any(), any(), any(), any(), any(), any()))
+            .thenReturn(SscsCaseDetails.builder().data(caseData).build());
+
+        service.mergeDocIntoCcd("Appellant statement 1 - SC0011111.pdf", new byte[0], 1L,
+            caseData, IdamTokens.builder().build(), "Other evidence");
+
+        verify(pdfStoreService, times(1))
+            .store(any(), eq("Appellant statement 1 - SC0011111.pdf"), eq("Other evidence"));
+
+        ArgumentCaptor<SscsCaseData> caseDataCaptor = ArgumentCaptor.forClass(SscsCaseData.class);
+        verify(ccdService, times(1))
+            .updateCase(caseDataCaptor.capture(), eq(1L), eq(UPLOAD_DOCUMENT.getCcdType()), anyString(),
+                anyString(), any(IdamTokens.class));
+
+        assertThat(caseDataCaptor.getValue().getScannedDocuments().size(), is(1));
+        Optional<ScannedDocument> scannedDocument = caseDataCaptor.getValue().getScannedDocuments().stream()
+            .filter(doc -> "Appellant statement 1 - SC0011111.pdf".equals(doc.getValue().getFileName()))
+            .findFirst();
+        if (scannedDocument.isPresent()) {
+            ScannedDocument expectedScannedDoc = ScannedDocument.builder()
+                .value(ScannedDocumentDetails.builder()
+                    .fileName("Appellant statement 1 - SC0011111.pdf")
+                    .url(DocumentLink.builder().documentUrl("http://dm-store").build())
+                    .type("other")
+                    .build())
+                .build();
+            assertThat(scannedDocument.get(), is(expectedScannedDoc));
+        } else {
+            fail("there must be one single doc expected");
+        }
+
+    }
+
+    private void mockPdfStoreService() {
+        SscsDocumentDetails sscsDocumentDetails = SscsDocumentDetails.builder()
+            .documentFileName("Appellant statement 1 - SC0011111.pdf")
+            .documentDateAdded(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+            .documentLink(DocumentLink.builder().documentUrl("http://dm-store").build())
+            .documentType("Other evidence")
+            .build();
+
+        SscsDocument pdfDocument = SscsDocument.builder().value(sscsDocumentDetails).build();
+
+        when(pdfStoreService.store(any(), any(), anyString())).thenReturn(singletonList(pdfDocument));
     }
 
     @Test
@@ -60,7 +130,7 @@ public class CcdPdfServiceTest {
         byte[] pdf = {};
         when(ccdService.updateCase(any(), any(), any(), any(), any(), any())).thenReturn(SscsCaseDetails.builder().data(caseData).build());
 
-        service.mergeDocIntoCcd("Myfile.pdf", pdf,1L, caseData, IdamTokens.builder().build(), "My description", "dl6");
+        service.mergeDocIntoCcd("Myfile.pdf", pdf, 1L, caseData, IdamTokens.builder().build(), "My description", "dl6");
 
         verify(pdfStoreService).store(any(), any(), eq("dl6"));
         verify(ccdService).updateCase(any(), any(), any(), eq("SSCS - upload document event"), eq("My description"), any());
