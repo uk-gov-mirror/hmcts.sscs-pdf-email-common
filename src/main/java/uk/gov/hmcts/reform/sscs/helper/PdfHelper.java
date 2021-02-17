ce4e73e5039aba1009afd0bde00ff6fac2dfab1c
Dan Hamilton <dan@emagineers.co.uk>
2021-02-17 14:53:54 +0000

    PDF Scaling

---
+++ src/main/java/uk/gov/hmcts/reform/sscs/helper/PdfHelper.java
@@ -0,0 +1,125 @@
+package uk.gov.hmcts.reform.sscs.helper;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.util.Matrix;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfHelper {
+
+    public boolean isDocumentWithinSize(PDDocument document, PDRectangle size) {
+
+        for (PDPage page: document.getPages()) {
+            float pageHeight = page.getMediaBox().getHeight();
+            float pageWidth = page.getMediaBox().getWidth();
+
+            if (pageHeight > pageWidth) {
+                if (pageHeight > size.getHeight() || pageWidth > size.getWidth()) {
+                    return false;
+                }
+            } else {
+                if (pageWidth > size.getHeight() || pageHeight > size.getWidth()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public BigDecimal calculateScalingFactor(PDDocument document, PDRectangle size) {
+
+        BigDecimal maxHeightScaling = new BigDecimal(0);
+        BigDecimal maxWidthScaling = new BigDecimal(0);
+        float heightOverage;
+        float widthOverage;
+
+        for (PDPage page: document.getPages()) {
+            float pageHeight = page.getMediaBox().getHeight();
+            float pageWidth = page.getMediaBox().getWidth();
+            float sizeHeight;
+            float sizeWidth;
+
+            if (pageHeight > pageWidth) {
+                sizeHeight = size.getHeight();
+                sizeWidth = size.getWidth();
+            } else {
+                sizeHeight = size.getWidth();
+                sizeWidth = size.getHeight();
+            }
+
+            System.out.println("height limit = " + sizeHeight);
+            System.out.println("width limit = " + sizeWidth);
+
+            heightOverage = pageHeight - sizeHeight;
+            System.out.println("height overage = " + heightOverage);
+            widthOverage = pageWidth - sizeWidth;
+            System.out.println("width overage = " + widthOverage);
+
+            maxHeightScaling = maxScaleFactor(heightOverage, sizeHeight, maxHeightScaling);
+            System.out.println("max height scaling = " + maxHeightScaling);
+            maxWidthScaling = maxScaleFactor(widthOverage, sizeWidth, maxWidthScaling);
+            System.out.println("max width scaling = " + maxWidthScaling);
+        }
+
+        BigDecimal scalingFactor = maxHeightScaling.compareTo(maxWidthScaling) > 0 ? maxHeightScaling : maxWidthScaling;
+
+        return  scalingFactor.setScale(4, RoundingMode.HALF_EVEN);
+    }
+
+    public BigDecimal maxScaleFactor(float overage, float pageAxisDimension, BigDecimal maxScaleFactor) {
+        if (overage > 0) {
+            BigDecimal scaleFactor = new BigDecimal(overage / pageAxisDimension);
+            maxScaleFactor = scaleFactor.compareTo(maxScaleFactor) > 0 ? scaleFactor : maxScaleFactor;
+        }
+        return  maxScaleFactor;
+    }
+
+    public PDDocument scaleDownDocumentToPageSize(PDDocument document, BigDecimal scaleDownFactor, PDRectangle size) throws Exception {
+        try {
+            for (PDPage page : document.getPages()) {
+                System.out.println("Page");
+                scaleContent(document, page, scaleDownFactor.floatValue());
+                PDRectangle newSize = page.getMediaBox().getHeight() > page.getMediaBox().getWidth() ? size : new PDRectangle(size.getHeight(), size.getWidth());
+                page.setMediaBox(newSize);
+                page.setCropBox(newSize);
+                page.setBleedBox(newSize);
+                page.setTrimBox(newSize);
+                page.setArtBox(newSize);
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return document;
+    }
+
+    /**
+     * This solution duplicates what the examples do in iText, which is to write a raw scaling command
+     * directly into the PDF, and then write a final command to cancel the scaling.
+     * However, PDFBox has actual calls to replicate this, you don't have to write raw commands.
+     */
+    private void scaleContent(PDDocument document, PDPage page, float percentage) throws IOException {
+
+        // First create a content stream before all others, add a matrix transformation to scale:
+        try (PDPageContentStream contentStream =
+                     new PDPageContentStream(document, page, PDPageContentStream.AppendMode.PREPEND, false)) {
+
+            contentStream.saveGraphicsState(); // 'q' in PDF commands
+            contentStream.transform(new Matrix(percentage, 0, 0, percentage, 0, 0));
+            contentStream.saveGraphicsState();
+        }
+
+        // Now add a closing command to remove the scale effect by restoring the graphics states:
+        try (PDPageContentStream contentStream =
+                     new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, false)) {
+            // In raw PDF this equates to: "\nQ\nQ\n" - we saved it twice so we have to restore twice
+            contentStream.restoreGraphicsState();
+            contentStream.restoreGraphicsState();
+        }
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/sscs/helper/PdfHelperTest.java
@@ -0,0 +1,123 @@
+package uk.gov.hmcts.reform.sscs.helper;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.URL;
+import org.apache.commons.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.junit.Test;
+
+public class PdfHelperTest {
+
+    PdfHelper pdfHelper = new PdfHelper();
+
+    @Test
+    public void returnFalseWhenPdfHasPageGreaterThanAllowedSizePortrait() {
+
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = new PDDocument();
+        document.addPage(new PDPage(new PDRectangle(pageSize.getWidth() + 1, pageSize.getHeight() + 1)));
+
+        boolean result = pdfHelper.isDocumentWithinSize(document, pageSize);
+        assertFalse(result);
+    }
+
+    @Test
+    public void returnFalseWhenPdfHasPageGreaterThanAllowedSizeLandscape() {
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = new PDDocument();
+        document.addPage(new PDPage(new PDRectangle(pageSize.getHeight() + 1, pageSize.getWidth() + 1)));
+
+        boolean result = pdfHelper.isDocumentWithinSize(document, pageSize);
+        assertFalse(result);
+    }
+
+    @Test
+    public void returnTrueWhenPdfHasNoPagesGreaterThanAllowedSizePortrait() {
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = new PDDocument();
+        document.addPage(new PDPage(new PDRectangle(pageSize.getWidth(), pageSize.getHeight())));
+
+        boolean result = pdfHelper.isDocumentWithinSize(document, pageSize);
+        assertTrue(result);
+    }
+
+    @Test
+    public void returnTrueWhenPdfHasNoPagesGreaterThanAllowedSizeLandscape() {
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = new PDDocument();
+        document.addPage(new PDPage(new PDRectangle(pageSize.getHeight(), pageSize.getWidth())));
+
+        boolean result = pdfHelper.isDocumentWithinSize(document, pageSize);
+        assertTrue(result);
+    }
+
+    @Test
+    public void returnCorrectMaxScalingFactorForOversizedPortraitWidth() {
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = new PDDocument();
+        document.addPage(new PDPage(new PDRectangle(pageSize.getWidth() + 100, pageSize.getHeight() + 50)));
+
+        BigDecimal result = pdfHelper.calculateScalingFactor(document, pageSize);
+        assertEquals(new BigDecimal(0.1680).setScale(4, RoundingMode.HALF_EVEN), result);
+    }
+
+    @Test
+    public void returnCorrectMaxScalingFactorForOversizedPortraitHeight() {
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = new PDDocument();
+        document.addPage(new PDPage(new PDRectangle(pageSize.getWidth() + 50, pageSize.getHeight() + 100)));
+
+        BigDecimal result = pdfHelper.calculateScalingFactor(document, pageSize);
+        assertEquals(new BigDecimal(0.1188).setScale(4, RoundingMode.HALF_EVEN), result);
+    }
+
+    @Test
+    public void scaleDownLandscapeDocumentCorrectly() throws Exception {
+
+        byte[] pdfBytes = IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("A3 Landscape.pdf"));
+
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = PDDocument.load(pdfBytes);
+
+        BigDecimal scaleDownFactor = new BigDecimal(0.5).setScale(4, RoundingMode.HALF_EVEN);
+
+        PDDocument result = pdfHelper.scaleDownDocumentToPageSize(document, scaleDownFactor, pageSize);
+
+        URL url = this.getClass().getClassLoader().getResource("/");
+        File file = new File(url + "out.pdf");
+        result.save(file);
+
+        PDRectangle mediaBox = result.getPage(0).getMediaBox();
+
+        assertTrue(pageSize.getHeight() >= mediaBox.getWidth());
+        assertTrue(pageSize.getWidth() >= mediaBox.getHeight());
+    }
+
+    @Test
+    public void scaleDownPortraitDocumentCorrectly() throws Exception {
+
+        byte[] pdfBytes = IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("A3 Portrait.pdf"));
+
+        PDRectangle pageSize = PDRectangle.A4;
+        PDDocument document = PDDocument.load(pdfBytes);
+
+        BigDecimal scaleDownFactor = new BigDecimal(0.5).setScale(4, RoundingMode.HALF_EVEN);
+
+        PDDocument result = pdfHelper.scaleDownDocumentToPageSize(document, scaleDownFactor, pageSize);
+
+        URL url = this.getClass().getClassLoader().getResource("/");
+        File file = new File(url + "out.pdf");
+        result.save(file);
+
+        PDRectangle mediaBox = result.getPage(0).getMediaBox();
+
+        assertTrue(pageSize.getHeight() >= mediaBox.getHeight());
+        assertTrue(pageSize.getWidth() >= mediaBox.getWidth());
+    }
+}
