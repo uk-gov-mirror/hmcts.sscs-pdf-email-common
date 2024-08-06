ab15bfcfdb244a0076312500463a793af0509c7c
Andrew McMahon <88711632+Andrew-McMahon7@users.noreply.github.com>
2024-08-06 11:08:37 +0000

    Sscssi 161 round2 (#1110)
    * Revert "Revert "Bump sscs-common version, change updateCaseV2 calls (#1106)" (#1108)"
    
    This reverts commit d3759a415c4dab686cd698a629441844c870e879.
    
    * Point to sscs-commons
    
    * Temporary suppression bump
    
    * SSCSSI-161: update from common merge
    
    * SSCSSI-161: update from common change
    
    * SSCSSI-161: upgraded common
    
    * SSCSSI-161: upgraded common
    
    * SSCSSI-161: rebuild
    
    * SSCSSI-161: upgraded common
    
    * SSCSSI-161: upgraded common2
    
    * SSCSSI-161: upgraded common to 5.10.0
    
    ---------
    
    Co-authored-by: sanjayparekh-hmcts <36845702+sanjayparekh-hmcts@users.noreply.github.com>
    Co-authored-by: sanjayparekh-hmcts <sanjay.parekh@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/sscs/service/CcdNotificationsPdfService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/CcdNotificationsPdfService.java
@@ -69,7 +69,8 @@ public class CcdNotificationsPdfService {
     public void mergeCorrespondenceIntoCcdV2(Long caseId, Correspondence correspondence) {
         List<Correspondence> updatedCorrespondences = getCorrespondences(correspondence);
 
-        Consumer<SscsCaseData> caseDataConsumer = caseData -> {
+        Consumer<SscsCaseDetails> caseDataConsumer = caseDetails -> {
+            SscsCaseData caseData = caseDetails.getData();
             List<Correspondence> existingCorrespondence = caseData.getCorrespondence() == null ? new ArrayList<>() : caseData.getCorrespondence();
             List<Correspondence> allCorrespondence = new ArrayList<>(existingCorrespondence);
             allCorrespondence.addAll(updatedCorrespondences);
@@ -116,7 +117,8 @@ public class CcdNotificationsPdfService {
     public void mergeLetterCorrespondenceIntoCcdV2(byte[] pdf, Long ccdCaseId, Correspondence correspondence, String senderType) {
         final List<Correspondence> correspondences = getCorrespondences(pdf, correspondence);
 
-        Consumer<SscsCaseData> caseDataConsumer = sscsCaseData -> {
+        Consumer<SscsCaseDetails> caseDataConsumer = sscsCaseDetails -> {
+            SscsCaseData sscsCaseData = sscsCaseDetails.getData();
             List<Correspondence> existingCorrespondence = sscsCaseData.getCorrespondence() == null ? new ArrayList<>() : sscsCaseData.getCorrespondence();
             List<Correspondence> allCorrespondence = new ArrayList<>(existingCorrespondence);
             allCorrespondence.addAll(correspondences);
@@ -182,7 +184,8 @@ public class CcdNotificationsPdfService {
     public void mergeReasonableAdjustmentsCorrespondenceIntoCcdV2(byte[] letterDocument, Long ccdCaseId, Correspondence correspondence, LetterType letterType) {
         List<Correspondence> correspondenceList = getCorrespondences(letterDocument, correspondence);
 
-        Consumer<SscsCaseData> caseDataConsumer = sscsCaseData -> {
+        Consumer<SscsCaseDetails> caseDataConsumer = sscsCaseDetails -> {
+            SscsCaseData sscsCaseData = sscsCaseDetails.getData();
             sscsCaseData.setReasonableAdjustmentsLetters(
                     buildCorrespondenceByParty(sscsCaseData, correspondenceList, letterType));
             sscsCaseData.updateReasonableAdjustmentsOutstanding();
